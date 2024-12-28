0465a082ce7b6eb1d597950538034b6880d92497
chiscookeke11 <chiscookeke11@gmail.com>
2024-12-28 09:34:32 -1100

    +

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -3,14 +3,72 @@ export const phones = [
         id:"1",
         name: "tecno note five",
         currentPrice: "70000",
-        oldPrice: "90000"
+        oldPrice: "90000",
+        image: "https://res.cloudinary.com/dwedz2laa/image/upload/v1735394389/phone1_guqgvy.png"
 
     },
     {
         id: "2",
-        name: "tecno note five",
-        currentPrice: "70000",
+        name: "tecno note six",
+        currentPrice: "9000",
         oldPrice: "90000"
 
-    }
+    },
+    {
+        id: "3",
+        name: "tecno note six",
+        currentPrice: "9000",
+        oldPrice: "90000"
+
+    },
+    {
+        id: "4",
+        name: "tecno note six",
+        currentPrice: "9000",
+        oldPrice: "90000"
+
+    },
+    {
+        id: "5",
+        name: "tecno note six",
+        currentPrice: "9000",
+        oldPrice: "90000"
+
+    },
+    {
+        id: "6",
+        name: "tecno note six",
+        currentPrice: "9000",
+        oldPrice: "90000"
+
+    },
+    {
+        id: "7",
+        name: "tecno note six",
+        currentPrice: "9000",
+        oldPrice: "90000"
+
+    },
+    {
+        id: "8",
+        name: "tecno note six",
+        currentPrice: "9000",
+        oldPrice: "90000"
+
+    },
+    {
+        id: "9",
+        name: "tecno note six",
+        currentPrice: "9000",
+        oldPrice: "90000"
+
+    },
+    {
+        id: "10",
+        name: "tecno note six",
+        currentPrice: "9000",
+        oldPrice: "90000"
+
+    },
+ 
 ]
--- src/components/SmartPhonesDisplay.jsx
+++ src/components/SmartPhonesDisplay.jsx
@@ -1,43 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaChevronRight } from 'react-icons/fa'
 import { phones } from '../assets/assets'
 
 const SmartPhonesDisplay = () => {
 
 
+  const [sliced, setSliced] = useState(5)
+
+  const increaseSlice = () => {
+    setSliced(prevSlice => Math.min(prevSlice + 5), phones.length )
+  }
+  const resetSlice = () => {
+    setSliced(5)
+  }
+
+
  
 
   return (
-    <div className='mx-auto mt-5 w-full bg-white px-[7%] text-sm lg:text-md min-h-[20vh]' >
+    <div className='mx-auto mt-5 w-full my-3 bg-white px-[7%] text-sm lg:text-md min-h-[20vh]' >
         <header className='flex flex-row items-center justify-between w-full py-3 border-b border-[#EDEDED] relative' >
             <h2>Grab the best deal on <span  className='text-[#008ECC] font-bold ' >  Smartphones</span></h2>
-            <h2 className='flex flex-row items-center gap-3 cursor-pointer'  ><span>See All</span>   <FaChevronRight  className='text-[#008ECC]'/> </h2>
+            <h2 className={`flex flex-row items-center gap-3 cursor-pointer ${sliced >= phones.length? "cursor-not-allowed" : "cursor-pointer" } `} onClick={increaseSlice}   ><span>See All</span>   <FaChevronRight  className='text-[#008ECC]'/> </h2>
           
           <hr className='absolute bottom-0 left-0 w-1/4 bg-[#008ECC] h-[2px]'  />
         </header>
 
-        <div className="display-box flex flex-row items-start justify-start py-5 px-2 ">
+        <div className="display-box grid grid-cols-auto gap-4 gap-y-6  flex-wrap py-5 px-2 ">
 
 
-        {phones.map((item, index)=> {
+        {phones.slice(0, sliced).map((item, index)=> {
 
 
           const discount = (item.oldPrice - item.currentPrice)
       
 
           const percentageDiscount = ((discount/ item.oldPrice) * 100 ).toFixed(0)
-          console.log(percentageDiscount)
+         
 
    
 
           
           return(
-            <div key={index} className="item w-[200px] h-[275px] min-w-[130px] min-h-[220px] bg-white rounded-[16px] flex flex-col justify-start gap-2 overflow-hidden border relative border-[#EDEDED] mx-2 cursor-pointer duration-300 hover:border-[#008ECC] transition-all transform hover:scale-105">
+            <div key={index} className="item w-[200px] h-[275px] min-w-[130px] min-h-[220px] bg-white rounded-[16px] flex flex-col justify-start gap-2 overflow-hidden border relative border-[#EDEDED] mx-auto my-3 cursor-pointer duration-300 hover:border-[#008ECC] transition-all transform hover:scale-105">
 
 
 
           <div className='h-2/3 w-full flex items-center justify-center bg-[#EDEDED] rounded-t-[16px] ' >
-            <img src="https://res.cloudinary.com/dwedz2laa/image/upload/v1735394389/phone1_guqgvy.png" alt="" className='w-[75px]'  />
+            <img src={item.image} alt="" className='w-[75px]'  />
           </div>
 
           <div className='px-3  ' >
@@ -66,6 +76,7 @@ const SmartPhonesDisplay = () => {
 
    
         </div>
+        <button onClick={resetSlice} className='mx-auto bg-red-500' >Hide </button>
     </div>
   )
 }
