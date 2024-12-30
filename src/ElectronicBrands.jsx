43b42219768a5bcd860bac2393ec21c8ab50763f
chiscookeke11 <chiscookeke11@gmail.com>
2024-12-30 00:25:38 -1100

    FIX: footer complete

--- src/ElectronicBrands.jsx
+++ src/ElectronicBrands.jsx
@@ -4,7 +4,7 @@ import Responsive from './ElectronicBrandSlider'
 
 const ElectronicBrands = () => {
   return (
-      <div className='mx-auto  w-full my-[5%] px-[7%] text-sm lg:text-base min-h-[20vh]' >
+      <div className='mx-auto  w-full my-[5%] px-[7%] text-base lg:text-base min-h-[20vh]' >
                 <header className='flex flex-row items-center justify-between w-full py-3 border-b border-[#EDEDED] relative' >
                     <h2>Top <span  className='text-[#008ECC] font-bold ' >Electronics Brands</span></h2>
                     <h2 className={`flex flex-row items-center gap-3 cursor-pointer `}   ><span>See All</span>   <FaChevronRight  className='text-[#008ECC]'/> </h2>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,12 +3,12 @@ import { FaPhone, FaWhatsapp } from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <div className='w-full bg-[#008ECC] px-[4%] py-[2%] flex flex-col gap-3 items-center justify-center ' >
+    <div className='w-full bg-[#008ECC] px-[4%] py-[2%] flex flex-col gap-3 items-center justify-center relative overflow-hidden ' >
 
         <div className=" w-full top flex flex-col lg:flex-row md:flex-row flex-wrap  justify-evenly gap-5 mb-[3%] ">
 
 
-            <div className='min-w-[320px]  text-white text-sm ' >
+            <div className='min-w-[320px]  text-white text-sm z-10 ' >
 
                 <h1 className='text-white text-4xl font-bold mb-[6%] ' >MegaMart</h1>
                 <h2 className='text-lg mb-[5%] ' >Contact Us</h2>
@@ -35,7 +35,8 @@ const Footer = () => {
                 <h2 className='text-lg mb-[5%] ' >Download App</h2>
 
                 <div className='w-full flex items-center gap-7 ' >
-                    <a href="#"><img src="" alt="" /></a>
+                    <a href="#"><img src="https://res.cloudinary.com/dwedz2laa/image/upload/v1735556281/app_store_vqqnbg.png" alt="" className='w-[120px] ' /></a>
+                    <a href="#"><img src="https://res.cloudinary.com/dwedz2laa/image/upload/v1735556281/gle_play_om3yea.png" alt="" className='w-[120px] ' /></a>
                 </div>
                 </div>
 
@@ -45,7 +46,7 @@ const Footer = () => {
             </div>
 
 
-            <div className='block text-white ' >
+            <div className='block text-white z-10' >
                 <h1  className='text-lg mb-[6%] py-3 border-b-2 border-white w-fit ' >Most Popular Categories</h1>
  
                 <ul className='my-4 list-disc px-2' >
@@ -64,7 +65,7 @@ const Footer = () => {
 
 
 
-            <div className='block text-white mx-2' >
+            <div className='block text-white mx-2 z-10' >
                 <h1  className='text-lg mb-[6%] py-3 border-b-2 border-white w-fit ' >Customer Services</h1>
 
                 <ul  className='my-4 list-disc px-2'>
@@ -135,6 +136,11 @@ const Footer = () => {
         <div className="bottom w-full text-center text-sm text-[#FFFFFF] font-[400]  border-t border-[#05ABF3] py-5" >
 
             <p>© 2022 All rights reserved. Reliance Retail Ltd.</p>
+        </div>
+
+
+        <div className="outer-circle absolute w-[200px] h-[200px] lg:w-[300px] lg:h-[300px] border-2 border-[#0C9BDA] rounded-[50%] top-[-10%] right-[-7%] z-0 flex items-center justify-center ">
+            <div className="inner-circle w-[90%] h-[90%] rounded-[50%] bg-[#0c9bda] "></div>
         </div>
 
     </div>
